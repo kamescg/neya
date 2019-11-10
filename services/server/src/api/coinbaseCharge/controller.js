c365fce2a2ddebe825df09e19e26521a1540f6ff
Kames <info@kamescg.com>
2019-11-10 07:57:48 -0500

    Performing Quick Save

--- services/server/src/api/coinbaseCharge/controller.js
+++ services/server/src/api/coinbaseCharge/controller.js
@@ -1,44 +1,20 @@
-import {sign} from '../../services/jwt';
-import {success} from '../../services/response/';
-import axios from 'axios';
 import coinbase from 'coinbase-commerce-node';
-var Checkout = coinbase.resources.Checkout;
-var Client = coinbase.Client;
+const Checkout = coinbase.resources.Checkout;
+const Client = coinbase.Client;
 
-Client.init('ad2a2d82-c0e8-41b0-bd84-ff29b7522f43');
-
-export const createCharge = ({body: {amount}}, res, next) => {
-  // console.log(req.amount);
-
+Client.init(env.process.COINBASE_API);
+export const createCharge = (req, res, next) => {
   var checkoutData = {
-    name: 'The Sovereign Individual',
-    description: 'Mastering the Transition to the Information Age',
+    name: req.body.name,
+    description: req.body.description,
     pricing_type: 'fixed_price',
     local_price: {
-      amount: amount,
+      amount: req.body.name,
       currency: 'USD',
     },
     requested_info: ['name', 'email'],
   };
   Checkout.create(checkoutData, function(error, response) {
-    // console.log(error);
-    // console.log(response);
-    res.send(response);
+    return res.json(response);
   });
 };
-
-// curl https://api.commerce.coinbase.com/checkouts \
-//      -X POST \
-//      -H 'Content-Type: application/json' \
-//      -H "X-CC-Api-Key: ad2a2d82-c0e8-41b0-bd84-ff29b7522f43" \
-//      -H "X-CC-Version: 2018-03-22" \
-//      -d '{
-//          "name": "The Sovereign Individual",
-//          "description": "Mastering the Transition to the Information Age",
-//          "local_price": {
-//              "amount": "100.00",
-//              "currency": "USD"
-//          },
-//        "pricing_type": "fixed_price",
-//          "requested_info": ["email"]
-//      }'
--- services/server/src/api/coinbaseCharge/index.js
+++ services/server/src/api/coinbaseCharge/index.js
@@ -1,23 +1,19 @@
-import axios from 'axios';
 import {Router} from 'express';
-import {middleware as query} from 'querymen';
 import bodyParser from 'body-parser';
-import {middleware as body} from 'bodymen';
 import {createCharge} from './controller';
 
 const router = new Router();
 
 /**
- * @api {post} /auth Authenticate
- * @apiName Authenticate
- * @apiGroup Auth
+ * @api {post} /create Create
+ * @apiName Create
+ * @apiGroup Checkout
  * @apiPermission master
  * @apiHeader {String} Authorization Basic authorization with email and password.
  * @apiParam {String} access_token Master access_token.
- * @apiSuccess (Success 201) {String} token User `access_token` to be passed to other requests.
- * @apiSuccess (Success 201) {Object} user Current user's data.
- * @apiError 401 Master access only or invalid credentials.
+ * @apiSuccess (Success 201) {Object} Checkout Objectreturn
+ * @apiError 401 Can't authenticate
  */
-router.post('/create', bodyParser.json(), createCharge);
+router.post('/create', bodyParser, createCharge());
 
 export default router;
